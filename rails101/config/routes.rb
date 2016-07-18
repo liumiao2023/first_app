b823179c75c3c5bc40f3b7ad677c7a46b9ce3c79
liumiao2023 <liumiao2023@hotmail.com>
2016-07-18 19:06:07 +0800

    use simple_format helper to decorate

---
+++ rails101/config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  devise_for :users
+
+  namespace :account do
+  resources :groups
+  resources :posts
+  end
+
+  resources :groups do
+    member do
+    post :join
+    post :quit
+  end
+    resources :posts
+  end
+  root 'groups#index'
+end
