d7285f9d5b4dfc3926c9f996795d137f67b56808
liumiao2023 <liumiao2023@hotmail.com>
2016-10-20 15:22:56 +0800

    changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :topics
-  root "topics#index" # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root "welcome#index" # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
